a343177d057b0f24a4400d2c6e0ee31c2641c67b
ericknovais <erick.hora@outlook.com>
2024-01-05 17:46:00 -0300

    Python: fanalização do curso

--- oo-sabor-express/app.py
+++ oo-sabor-express/app.py
@@ -5,6 +5,10 @@ restaurante_bk = Restaurante('bk','fostfood')
 restaurante_mexicano = Restaurante('MexiCuzinho', 'Mexicano') 
 restaurante_bk.alternar_estado()
 
+restaurante_bk.receber_avalicao('Erick', 7)
+restaurante_bk.receber_avalicao('Roberta', 5)
+restaurante_bk.receber_avalicao('Jamal', 8)
+
 def main():
     Restaurante.listar_restaurantres()
 

96d3c3d4c5297cc6db8e4c9e5b73ea10ab6a0090
ericknovais <erick.hora@outlook.com>
2024-02-20 21:28:34 -0300

    Desafios

--- data-science/Atividade7.py
+++ data-science/Atividade7.py
@@ -1,3 +1,9 @@
+'''
+    7. Você recebeu uma demanda para gerar números de token para acessar o aplicativo de uma empresa. 
+    O token precisa ser par e variar de 1000 até 9998. 
+    Escreva um código que solicita à pessoa usuária o seu nome e exibe uma mensagem junto a esse 
+    token gerado aleatoriamente.
+'''
 from random import choice,randrange,randint
 
 nome = input('Informe seu nome:')
